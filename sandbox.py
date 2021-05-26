fb274d642a6fbaf7bd7b1a3fbe92594323413c10
Rai <julian.magnabosco@gmail.com>
2021-05-26 00:11:28 -0300

    agregue un boton

--- sandbox.py
+++ sandbox.py
@@ -29,6 +29,9 @@ mode = 2
 
 screen.fill(colorbg)
 clock = pygame.time.Clock()
+
+pygame.font.init()
+
 pixels = pygame.PixelArray(game_screen)
 check = 5
 def checkPos(x, y, newtiles):
@@ -64,7 +67,36 @@ def draw():
             # pygame.draw.rect(screen, color, pos)
             pixels[x][y] = color
 
+class Button :
+    def __init__(self, text,  pos, font, bg="black", feedback=""):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Arial", font)
+        if feedback == "":
+            self.feedback = "text"
+        else:
+            self.feedback = feedback
+        self.change_text(text, bg)
 
+    def change_text(self, text, bg="black"):
+        """Change the text when you click"""
+        self.text = self.font.render(text, 1, pygame.Color("White"))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def show(self):
+        screen.blit(self.surface, (self.x, self.y))
+
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
+
+button1 = Button("Arena",(size[0]+8,20),20,bg=colors[1],feedback="hola")
 draw()
 pygame.display.flip()
 register = False
@@ -82,7 +114,7 @@ while True:
                 painting = 0
             if event.button == 3:
                 register = 0
-    
+        button1.click(event)
     posM = list(pygame.mouse.get_pos())
     posM[0] = int((posM[0])/tSize)
     posM[1] = int((posM[1])/tSize)
@@ -103,4 +135,6 @@ while True:
     clock.tick(FPS)
     # pygame.transform.scale(game_screen, screen.get_size())
     screen.blit(pygame.transform.scale(game_screen, size), (0, 0))
+    button1.show()
+
     pygame.display.flip()
